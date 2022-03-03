1d949eb168b12f1b822220b5b91ab29e1c63df29
Samuel Pradhan <samuel.pradhan22@gmail.com>
2022-03-03 14:55:58 +0600

    routing configuration

---
+++ resources/views/registration/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+    <div class="card">
+        <div class="card-header">
+          Register for Quarantine Facilit
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">Test</h5>
+          <p class="card-text">Need to desgin registration form here</p>
+        </div>
+      </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
+Route::get('/', [App\Http\Controllers\Registration\RegistrationController::class, 'index'])->name('registration.index');
 
 Auth::routes();
 
-Route::get('/', [App\Http\Controllers\DashboardController::class, 'dashboard'])->name('dashboard');
+Route::get('/dashboard', [App\Http\Controllers\DashboardController::class, 'dashboard'])->name('dashboard');
