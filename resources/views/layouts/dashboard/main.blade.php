41936d037ddf430718f2be3464b4659b738da938
Samuel Pradhan <samuel.pradhan22@gmail.com>
2022-03-03 23:44:51 +0600

    changed dashboard

--- resources/views/layouts/dashboard/main.blade.php
+++ resources/views/layouts/dashboard/main.blade.php
@@ -25,9 +25,9 @@
     <script src="{{asset('assets/js/all.min.js')}}" crossorigin="anonymous"></script>
 </head>
 <body class="sb-nav-fixed">
-  <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
+  <nav class="sb-topnav navbar navbar-expand navbar-dark bg-primary">
       <!-- Navbar Brand-->
-      <a class="navbar-brand ps-3" href="index.html">{{ config('app.name', 'QMS') }}</a>
+      <a class="navbar-brand ps-3" href="{{route('dashboard')}}">{{ config('app.name', 'QMS') }}</a>
       <!-- Sidebar Toggle-->
       <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
       <!-- Navbar Search-->
@@ -61,11 +61,11 @@
   </nav>
   <div id="layoutSidenav">
       <div id="layoutSidenav_nav">
-          <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+          <nav class="sb-sidenav accordion sb-sidenav-light" id="sidenavAccordion">
               <div class="sb-sidenav-menu">
                   <div class="nav">
                       <div class="sb-sidenav-menu-heading">Core</div>
-                      <a class="nav-link" href="index.html">
+                      <a class="nav-link" href="{{ route("dashboard")}}">
                           <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                           Dashboard
                       </a>
@@ -104,7 +104,7 @@
           <footer class="py-4 bg-light mt-auto">
               <div class="container-fluid px-4">
                   <div class="d-flex align-items-center justify-content-between small">
-                      <div class="text-muted">Copyright &copy; RGoB 2021</div>
+                      <div class="text-muted">Copyright &copy; RGoB 2022</div>
                   </div>
               </div>
           </footer>
